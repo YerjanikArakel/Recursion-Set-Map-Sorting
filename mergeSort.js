42daba70e2258bf2965a4b3852a97808512c67a3
Yerjanik Arakel <arakelyanyerjanik@gmail.com>
2020-07-12 17:46:52 +0400

    relized merge sorting in mergeSort.js, after export it in minPositiveElem.js and easily solve the exercise with some hack

---
+++ mergeSort.js
@@ -0,0 +1,35 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const middle = Math.floor(arr.length / 2);
+
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let finalResult = [],
+    leftIndex = 0,
+    rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      finalResult.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      finalResult.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  return finalResult
+    .concat(left.slice(leftIndex))
+    .concat(right.slice(rightIndex));
+}
+const numbers_1 = [56, -9, 87, -23, 0, -105, 55, 150];
+console.dir(mergeSort(numbers_1));
+module.exports = mergeSort;
--- minPositiveElem.js
+++ minPositiveElem.js
@@ -1,3 +1,28 @@
-const numbers_1 = [56, -9, 87, -23, 0, -105, 55, 1];
+const numbers_1 = [56, -9, 87, -23, 0, -105, 55, 150];
 const numbers_2 = [45, -9, 15, 5, -78];
 const numbers_3 = [-5, -9, -111, -1000, -7];
+
+const mergeSort = require("./mergeSort");
+
+function minPositivElem(arr) {
+  const res = mergeSort(arr);
+
+  function init(res, min = -1) {
+    if (res.length === 0) {
+      return min;
+    }
+
+    const [first, ...rest] = res;
+
+    if (first < 0) {
+      return init(rest, min);
+    } else {
+      min = first;
+      return min;
+    }
+  }
+  return init(res);
+}
+
+console.dir(minPositivElem(numbers_1));
+console.dir(mergeSort(numbers_3));
