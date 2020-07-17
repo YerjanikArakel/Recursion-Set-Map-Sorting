ad0f482bd893cdfa3777528f4bb3cc52a7ccf87c
Yerjanik Arakel <arakelyanyerjanik@gmail.com>
2020-07-18 00:00:31 +0400

    create a function shiftRec and run it in some cases

---
+++ shiftRec.js
@@ -0,0 +1,14 @@
+function shiftRec(arr) {
+  function removeFirst(arr) {
+    const [first, ...rest] = arr;
+    if (arr.length === 0) {
+      return [];
+    }
+    return rest;
+  }
+  return removeFirst(arr);
+}
+
+console.dir(shiftRec([6, 78, "n", 0, 1]));
+console.dir(shiftRec([5]));
+console.dir(shiftRec([]));
