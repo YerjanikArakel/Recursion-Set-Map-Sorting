e874f8227a872f90195a71e1b3bd460b951e92a1
Yerjanik Arakel <arakelyanyerjanik@gmail.com>
2020-07-18 01:33:51 +0400

    created nestedArrConvert.js file with working function

---
+++ nestedArrConvert.js
@@ -0,0 +1,22 @@
+function nestedArrConvert(arr) {
+  function arrConvert(arr, res) {
+    if (arr.length === 0) {
+      return res;
+    }
+
+    const [first, ...rest] = arr;
+
+    if (typeof first === "number") {
+      res.push(first);
+    } else {
+      arrConvert(first, res);
+    }
+
+    return arrConvert(rest, res);
+  }
+
+  return arrConvert(arr, (res = []));
+}
+
+console.dir(nestedArrConvert([1, [3, 4, [1, 2]], 10]));
+console.dir(nestedArrConvert([14, [1, [[[3, []]], 1], 0]]));
